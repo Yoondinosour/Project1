6142937b232180847fd4e9b278e4fe00aa54c824
Yoonseayoung <yoondinosour@naver.com>
2022-03-20 23:31:37 +0900

    슬라이드 추가

--- javascript/jq.js
+++ javascript/jq.js
@@ -1,3 +1,15 @@
+/* header */
+const box1=document.querySelector('li.quick1');
+box1.addEventListener('click',()=>{
+    box1.classList.toggle('on')
+})
+
+const box2=document.querySelector('li.quick2');
+box2.addEventListener('click',()=>{
+    box2.classList.toggle('on')
+})
+
+/* top button */
 const btt=$('#back_to_top');
 $(window).scroll(function(){
     if($(window).scrollTop()>300){
@@ -11,3 +23,34 @@ btt.click(function(e){
     e.preventDefault();
     $('html,body').animate({scrollTop:0},500);
 })
+
+/* slide */
+$(document).ready(function(){
+    /* news */
+    $('.main_content3_board').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 2000,
+    });
+    /* sns */
+    $('.sns_contents_wrap').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 2000,
+    });
+    /* board(alim) */
+    $('.content3_board').slick({
+        autoplay: true,
+        autoplaySpeed: 2500,
+        infinite: true,
+        speed: 1500,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
